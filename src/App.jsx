8441ff0da951a17ecd65135940d8c69cb7d09094
Andrehlb <andreh.72@hotmail.com>
2025-05-04 15:43:37 -0300

    feat: add navigation links to the main page in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 // src/App.jsx
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link, Navigate } from 'react-router-dom';
+// import { Routes, Route } from 'react-router-dom'
 import CadastroEvento from './pages/eventos/CadastroEvento'
-import { Navigate } from 'react-router-dom';
+// import { Navigate } from 'react-router-dom';
 
 export default function App() {
   return (
+    <>
+    <nav>
+      <ul>
+        <li><Link to="/">Página Inicial</Link></li>
+        <li><Link to="/eventos/cadastro">Cadastro de Evento</Link></li>
+      </ul>
+    </nav>
+    
     <Routes>
+      <Route path="/" element={<div>Página Inicial</div>} />
       <Route path="/eventos/cadastro" element={<CadastroEvento />} />
       <Route path="/" element={<div>Página Inicial</div>} />
       <Route path="*" element={<Navigate to="/eventos/cadastro" />} />
     </Routes>
-  )
+    </>
+  );
 }
