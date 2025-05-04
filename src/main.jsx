b9d246deda98379701997a6f820324a53e1391dc
Andrehlb <andreh.72@hotmail.com>
2025-05-04 13:37:00 -0300

    refactor: clean up main.jsx by removing unused imports and commented code

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,30 @@
 // src/main.jsx
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import CadastroEvento from './pages/eventos/CadastroEvento'
+import ReactDOM from 'react-dom/client';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import App from './App.jsx';
+// import CadastroEvento from './pages/eventos/CadastroEvento'
+import './index.css';
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+/*
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/eventos/cadastro" element={<CadastroEvento />} />
-        {/* Adicione outras rotas aqui: fallback ou página inicial */}
+        {/* Adicione outras rotas aqui: fallback ou página inicial */ //}
         {/* <Route path="/" element={<Home />} /> */}
         {/* <Route path="*" element={<NotFound />} /> */}
-      </Routes>
-    </Router>
-  )
-}
+    //  </Routes>
+   // </Router>
+ //  )
+// }
 
 /*
 import ReactDOM from 'react-dom/client'
