e69b82d555ab8a468c59fa4739b1724a3941f40f
Andrehlb <andreh.72@hotmail.com>
2025-05-04 13:01:23 -0300

    refactor: reorganize main.jsx structure and improve routing setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,24 @@
 // src/main.jsx
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CadastroEvento from './pages/eventos/CadastroEvento'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/eventos/cadastro" element={<CadastroEvento />} />
+        {/* Adicione outras rotas aqui: fallback ou página inicial */}
+        {/* <Route path="/" element={<Home />} /> */}
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </Router>
+  )
+}
+
+/*
 import ReactDOM from 'react-dom/client'
+
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
@@ -12,3 +30,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode>,
 )
+*/
