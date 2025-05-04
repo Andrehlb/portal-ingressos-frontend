2aa98cce43cd5642184d16fa45c909071341156e
Andrehlb <andreh.72@hotmail.com>
2025-05-04 14:29:51 -0300

    feat: add routing for home page and redirect to CadastroEvento

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import CadastroEvento from './pages/eventos/CadastroEvento'
+import { Navigate } from 'react-router-dom';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/eventos/cadastro" element={<CadastroEvento />} />
+      <Route path="/" element={<div>Página Inicial</div>} />
+      <Route path="*" element={<Navigate to="/eventos/cadastro" />} />
     </Routes>
   )
 }
