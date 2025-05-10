8b379a9bde45c05c1230d2141b1317e254f818ba
Andrehlb <andreh.72@hotmail.com>
2025-05-10 08:33:30 -0300

    chore: downgrade tailwindcss to version 3.4.1 and update configuration files
    - Downgraded tailwindcss from 4.1.6 to 3.4.1 in package.json.
    - Refactored tailwind.config.js to use ES module syntax and cleared content array.
    - Added postcss.config.js to configure Tailwind CSS and Autoprefixer plugins.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
-// tailwind.config.js
-module.exports = {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}", // Isso permite o Tailwind escanear seus componentes React
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
