f6450e9110f380e22e4cfea91c6129c8a16e1f51
Andrehlb <andreh.72@hotmail.com>
2025-05-11 20:55:36 -0300

    fix: wrap App component with CartProvider for shopping cart context

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
 // src/main.jsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 // import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import App from './App.jsx';
-//import App from './AppTeste.jsx';
+import App from './App';
 // import CadastroEvento from './pages/eventos/CadastroEvento'
 import './index.css';
+import { CartProvider } from './context/CartContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
